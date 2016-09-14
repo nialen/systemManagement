09456ccb8685e8b4bdbda788bae05f590c20756c
dingshaohua <345561171@qq.com>
2016-09-14 15:19:29 +0800

    统一确定，取消按钮样式，排序方式；重置密码操作；

--- page/userMan/userMan.js
+++ page/userMan/userMan.js
@@ -5,7 +5,28 @@
 
 angular
 	.module('staffManModule', ['ui.bootstrap'])
-	.run(['$rootScope', function($rootScope) {
+	.run(['$rootScope', '$http', function($rootScope, $http) {
+		// $http请求样例
+		var obj = {
+			'operationSpecTypeCd': '1', //类型编码
+			'operationSpecTypeName': '', //权限类型名称
+			'requirePaging': 'true', //是否需要分页
+			'currentPage': '1', //当前页
+			'rowNumPerPage': '10', //每页展示行数
+			'totalRowNum': '0' //总行数
+		};
+		$http({
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;'
+			},
+			url: 'http://192.168.16.161:80/psm/privilege/profile/queryOperationSpecType.action',
+			data: 'param=' + JSON.stringify(obj)
+		}).then(function successCallback(response) {
+			// TODO 设置$rootScope
+		}, function errorCallback(response) {
+			// 获取数据接口出错
+		});
 		$rootScope.staffManResultList = []; // 查询员工列表
 		$rootScope.modifiedStaffMan = {}; // 待修改的员工信息
 		$rootScope.isForbidSubmit = true; // 禁用编辑员工提交按钮
@@ -46,14 +67,38 @@ angular
 	}])
 	// 查询结果控制器
 	.controller('staffManResultCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
-		// 修改
-		$scope.editStaffMan = function(index) {
-			$rootScope.modifiedStaffMan = $rootScope.staffManResultList[index];
-			parent.angular.element(parent.$('#tabs')).scope().addTab('修改用户', '/page/modifyStaff/modifyStaff.html', 'modifyStaff', JSON.stringify($rootScope.modifiedStaffMan));
-		}
 		// 新建
 		$scope.addStaffMan = function() {
-			parent.angular.element(parent.$('#tabs')).scope().addTab('新建用户', '/page/modifyStaff/modifyStaff.html', 'addNewStaff');
+				parent.angular.element(parent.$('#tabs')).scope().addTab('新建用户', '/page/modifyStaff/modifyStaff.html', 'addNewStaff');
+			}
+			// 修改
+		$scope.editStaffMan = function(index) {
+				$rootScope.modifiedStaffMan = $rootScope.staffManResultList[index];
+				parent.angular.element(parent.$('#tabs')).scope().addTab('修改用户', '/page/modifyStaff/modifyStaff.html', 'modifyStaff', JSON.stringify($rootScope.modifiedStaffMan));
+			}
+			// 密码重置
+		$scope.resetPassword = function(index) {
+
+			swal({
+				title: "密码重置",
+				text: "您确定要重置 " + $rootScope.staffManResultList[index].staffName + " 的密码为6个8吗？",
+				type: "warning",
+				showCancelButton: true,
+				closeOnConfirm: false,
+				confirmButtonText: "确定",
+				confirmButtonColor: "#ffaa00",
+				cancelButtonText: "取消",
+
+			}, function() {
+				$.ajax({
+					url: "",
+					type: "DELETE"
+				}).done(function(data) {
+					swal("操作成功!", "密码重置成功！", "success");
+				}).error(function(data) {
+					swal("OMG", "密码重置操作失败!", "error");
+				});
+			});
 		}
 	}])
 	// 分页控制器
