b920710738fac1261c1746af01b9fa4635373a21
聂亚兰 <3242662985@qq.com>
2016-10-24 09:30:29 +0800

    样式修改

--- js/httpConfig.js
+++ js/httpConfig.js
@@ -1,7 +1,7 @@
 define(function () {
     var httpConfig = {
-        // 'siteUrl': 'http://192.168.74.17/psm',
-        'siteUrl': 'http://192.168.16.161:80/psm',
+        'siteUrl': 'http://192.168.74.17/psm',
+        //'siteUrl': 'http://192.168.16.161:80/psm',
         // 'siteUrl': 'http://192.168.16.67:8080/psm',
         'requestHeader': {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
